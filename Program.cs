db7a3d440c2e234c6e9679b8754afb6834e5570a
«airapetyan <alexswatcorp98@mail.ru>
2023-01-17 14:28:29 +0300

    mess1

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Clear(); 
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Console.WriteLine("Введите число A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int step = a;
+// for (int i = 1; i < b; i++)
+// {
+// step = step * a;
+// }
+// Console.WriteLine("A в степени B = " + step);
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+//  while (n > 0)
+// {
+//     int num = n % 10;
+//     n = n / 10;
+//     sum = sum + num;
+// }
+// Console.WriteLine(sum);
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, которые вводит пользователь,
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+// Console.WriteLine("Введите числа: ");
+// int n = 8;
+// int [] array = new int[n];{
+// for (int i = 0; i < n; i++) 
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+
+
+
+            
